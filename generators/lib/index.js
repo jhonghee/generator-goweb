1ca8a6cbef4d6712fd8c8bde55f920d193464992
Jhonghee Park <jhonghee@gmail.com>
2015-08-31 19:28:43 -0400

    Moved lib layout to subcommand

---
+++ generators/lib/index.js
@@ -0,0 +1,47 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+
+module.exports = yeoman.generators.Base.extend({
+  prompting: function() {
+    var done = this.async();
+
+    var prompts = [{
+      name: 'import',
+      message: 'What is the import path of your project?',
+      default: 'github.com/yourname/pkgname'
+    }];
+
+    this.prompt(prompts, function(props){
+      this.props = props; // To access props later use this.props.import;
+
+      done();
+    }.bind(this));
+  },
+
+  writing: {
+    app: function() {
+      var pkgname = this.props.import.split('/').reverse()[0];
+      this.fs.copyTpl(
+        this.templatePath('_main.go'),
+        this.destinationPath('src/'+this.props.import+'/cmd/command/main.go'),
+        { projectImport: this.props.import,
+          packageName: pkgname}
+      );
+
+      this.fs.copyTpl(
+        this.templatePath('_lib.go'),
+        this.destinationPath('src/'+this.props.import+'/lib.go'),
+        { packageName: pkgname}
+      );
+    },
+
+    projectfiles: function() {
+      this.fs.copy(
+        this.templatePath('.gitignore'),
+        this.destinationPath('.gitignore')
+      );
+    }
+  }
+});
